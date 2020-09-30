c9807d3e67d870a944d14d2f3ce7f036fe927a70
Arden Sinclair <general@ardensinclair.com>
2020-09-30 00:01:04 -0600

    Added code for storing game data and printing it

--- RPGGame/__main__.py
+++ RPGGame/__main__.py
@@ -1,17 +1,4 @@
-from typing import List
-
-
-def num_list(name: str, list: List[str]):
-    """Prints numerical list with one item per line.
-
-    Args:
-        name: The name of the list.
-        list: The list of values.
-    """
-    print(f"{name}:")
-    for index, value in enumerate(list):
-        print(f"{index + 1}: {value.capitalize()}")
-
+from .GameData import printAllData
 
 if __name__ == "__main__":
-    print("Hello World")
+    printAllData()
