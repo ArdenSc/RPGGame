05a72c927c8f22d6b681515b7992cdf9b90b23f0
Arden Sinclair <general@ardensinclair.com>
2020-10-06 23:01:32 -0600

    Removed redundant code

--- RPGGame/GameWindow.py
+++ RPGGame/GameWindow.py
@@ -1,7 +1,4 @@
-from msvcrt import getch, kbhit
-from os import get_terminal_size, path, system
-from time import sleep, time
-
+from os import path, system
 from .GameState import GameState
 from .Menu import Menu
 from .strings.consoleAlign import *
@@ -12,72 +9,11 @@ class GameWindow:
         self._state = state
 
     @staticmethod
-    def terminalAlign():
-        if get_terminal_size().columns != 140 or get_terminal_size(
-        ).lines != 35:
-            system("cls")
-            print(STRING_RESIZE_NEEDED)
-            sleep(2)
-
-        if get_terminal_size().columns != 140:
-            old_width_state = width_state = ""
-            while not width_state == "done":
-                while get_terminal_size().columns != 140:
-                    old_width_state = width_state
-                    width_state = "Increase" if get_terminal_size(
-                    ).columns < 140 else "Decrease"
-                    if old_width_state != width_state:
-                        system("cls")
-                        print(STRING_WIDTH_INCORRECT.format(width_state))
-                time_end = time() + 2
-                system("cls")
-                print(STRING_WIDTH_CORRECT)
-                while time() < time_end:
-                    if get_terminal_size().columns != 140:
-                        old_width_state = width_state = ""
-                        break
-                else:
-                    width_state = "done"
-                    system("cls")
-                    print(STRING_WIDTH_DONE)
-                    sleep(3)
-
-        if get_terminal_size().lines != 35:
-            old_height_state = height_state = ""
-            while not height_state == "done":
-                while get_terminal_size().lines != 35:
-                    old_height_state = height_state
-                    height_state = "Increase" if get_terminal_size(
-                    ).lines < 35 else "Decrease"
-                    if old_height_state != height_state:
-                        system("cls")
-                        print(STRING_HEIGHT_INCORRECT if height_state ==
-                              "Increase" else STRING_HEIGHT_SKIPPABLE)
-                    if height_state == "Decrease" and kbhit() and getch(
-                    ) == b's':
-                        height_state = "done"
-                        break
-                if height_state == "done":
-                    system("cls")
-                    print(STRING_HEIGHT_DONE)
-                    sleep(3)
-                else:
-                    system("cls")
-                    print(STRING_HEIGHT_CORRECT)
-                    time_end = time() + 2
-                    while time() < time_end:
-                        if get_terminal_size().lines != 35:
-                            old_height_state = height_state = ""
-                            break
-                    else:
-                        height_state = "done"
-                        system("cls")
-                        print(STRING_HEIGHT_DONE)
-                        sleep(3)
+    def terminalResize(columns: int, lines: int):
+        system(f"mode con: cols={columns} lines={lines}")
 
     def run(self):
-        # Guided console window alignment
-        self.terminalAlign()
+        self.terminalResize(135, 35)
         map = open(
             path.join(path.dirname(__file__), 'assets', 'mapplaceholder.txt'))
         menu = Menu(map=map.readlines())
